bece23a8c8e446d47618c60b2916fc00c053b870
Quentin Rider <qdrider@gmail.com>
2019-05-14 12:27:11 -0600

    first commit

---
+++ src/components/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { Router, Route, Link } from 'svero';
+
+  import Test1 from './Test1.svelte';
+  import Test2 from './Test2.svelte';
+  import Index from './Index.svelte';
+</script>
+
+<Router>
+  <Route path="*" component={Index} />
+  <Route path="/test1" component={Test1} />
+  <Route path="/test2" component={Test2} />
+</Router>
+
+
+<ul>
+  <li>
+    <Link href="/test1" className="btn">Test1</Link>
+  </li>
+  <li>
+    <Link href="/test2" className="btn">Test2</Link>
+  </li>
+</ul>
+
+
---
+++ src/components/Index.svelte
@@ -0,0 +1,11 @@
+<script>
+  let adjective = 'Index';
+</script>
+
+<h1>{adjective} Page</h1>
+
+<style>
+  h1 {
+    color: tomato;
+  }
+</style>
